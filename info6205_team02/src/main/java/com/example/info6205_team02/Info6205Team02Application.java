ff6d8570db7ae14419f33ad85962acf9fa8e7e77
Shivani Datar <shivanidatar@Shivanis-MacBook-Pro.local>
2023-04-14 11:56:33 -0400

    Inital project structure

---
+++ info6205_team02/src/main/java/com/example/info6205_team02/Info6205Team02Application.java
@@ -0,0 +1,12 @@
+package com.example.info6205_team02;
+
+
+public class Info6205Team02Application {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+
+
+}
---
+++ info6205_team02/src/test/java/com/example/info6205_team02/Info6205Team02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.info6205_team02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class Info6205Team02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

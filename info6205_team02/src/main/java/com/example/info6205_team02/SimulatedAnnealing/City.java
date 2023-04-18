331f559186c1439b2e4f62b5da9e863a3b758000
Ashityagi <tyagi.as@northeastern.edu>
2023-04-17 22:06:51 -0400

    added code comments

--- info6205_team02/src/main/java/com/example/info6205_team02/SimulatedAnnealing/City.java
+++ info6205_team02/src/main/java/com/example/info6205_team02/SimulatedAnnealing/City.java
@@ -1,30 +1,40 @@
+//The code defines a class called "City" which represents a city on a map with its longitude and latitude coordinates.
 package com.example.info6205_team02.SimulatedAnnealing;
 
+//These lines declare the instance variables of the class "City",
+// which are the longitude (x), latitude (y) and crimeIds (c) for the city.
 public class City {
     private double x;
     private double y;
     private int c;
-
+//This is a constructor of the class "City" that initializes the instance variables with the given arguments.
     public City(int c,double x,double y) {
         this.c = c;
         this.x = x;
         this.y = y;
     }
-
+//This method calculates the distance between two cities using the haversine formula,
+// which takes into account the curvature of the earth. It takes two City objects as input parameters,
+// representing the two cities to calculate the distance between
     public double distanceToCity(City city1, City city2) {
+        //These lines define the radius of the earth in kilometers and convert the longitude and
+        // latitude values of each city from degrees to radians, which is required for the haversine formula.
         double radius = 6371;
         double x1 = city1.x*Math.PI/180;
         double y1= city1.y*Math.PI/180;
         double x2 = city2.x*Math.PI/180;
         double y2= city2.y*Math.PI/180;
+        //These lines calculate the distance between the two cities using the haversine formula, which takes into account the
+        //curvature of the earth. The calculated distance is returned as a double value.
         double deltaLat = x2-x1;
         double deltaLon = y2-y1;
         double a = Math.pow(Math.sin(deltaLat/2),2)+(Math.cos(x1)*Math.cos(x2)) * Math.pow(Math.sin(deltaLon/2),2);
         double c = 2 *Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
-        double distance = (radius * c)*1000;
+        double distance = (radius * c);
         return distance;
     }
-
+//These are getter and setter methods for the instance variables of the "City" class,
+// which provide access to the values of these variables and allow them to be modified.
     public double getX() {
         return x;
     }
--- info6205_team02/src/main/java/com/example/info6205_team02/SimulatedAnnealing/Travel.java
+++ info6205_team02/src/main/java/com/example/info6205_team02/SimulatedAnnealing/Travel.java
@@ -5,12 +5,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-
+//This line defines a public class called "Travel" which contains two private variables: "travel" and "previousTravel".
+// "travel" is an ArrayList of objects of type "City", which represents the order in which the cities are visited.
+// "previousTravel" is another ArrayList of "City" objects that keeps track of the previous order of cities.
 public class Travel {
     private ArrayList<City> travel = new ArrayList<>();
     private ArrayList<City> previousTravel = new ArrayList<>();
 
-
+//This code defines a constructor for the "Travel" class that takes a file path as an argument.
+// It reads in the data from the file and creates a new "City" object for each line of data.
+// It then adds each "City" object to the "travel" ArrayList.
     public Travel(String filePath) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(filePath));
         String line;
@@ -27,12 +31,14 @@ public class Travel {
             count ++;
         }
     }
-
+//This code randomly shuffles the order of the cities in the "travel" ArrayList.
     public void generateInitialTravel() {
         Collections.shuffle(travel);
     }
 
-
+//This code swaps the position of two random cities in the "travel" ArrayList.
+// It does this by generating two random indexes and swapping the "City" objects at those indexes.
+// It also saves the previous order of cities in the "previousTravel" ArrayList.
     public void swapCities() {
         int a = generateRandomIndex();
         int b = generateRandomIndex();
@@ -42,19 +48,26 @@ public class Travel {
         travel.set(a, y);
         travel.set(b, x);
     }
-
+//This code reverts the last swap by restoring the order of cities in the "travel" ArrayList
+// to the previous order stored in the "previousTravel" ArrayList.
     public void revertSwap() {
         travel = previousTravel;
     }
-
+//This code generates a random index within the bounds of the "travel" ArrayList.
     private int generateRandomIndex() {
         return (int) (Math.random() * travel.size());
     }
-
+//This code returns the "City" object at a specific index in the "travel" ArrayList.
     public City getCity(int index) {
         return travel.get(index);
     }
-
+//This method calculates the total distance of the current travel plan.
+// It initializes the distance variable to 0 and then iterates over each city in the travel list.
+// For each city, it gets the starting city and determines the destination city.
+// If the current city is not the last city in the travel list, then the destination city is the next
+// city in the list. Otherwise, the destination city is the first city in the list.
+// It then adds the distance between the starting and destination cities to the distance variable.
+// Finally, it returns the total distance of the travel plan.
     public double getDistance() {
         int distance = 0;
         for (int index = 0; index < travel.size(); index++) {
@@ -70,11 +83,11 @@ public class Travel {
 
         return distance;
     }
-
+    //This method returns the list of cities in the current travel plan.
     public ArrayList<City> getTravel() {
         return travel;
     }
-
+    //This method sets the list of cities in the current travel plan to the given travel list.
     public void setTravel(ArrayList<City> travel) {
         this.travel = travel;
     }
